9414b6ca17f6ca5718cb59008fbafe9d49dd58ba
merethomas7 <merediththomas972@gmail.com>
2017-07-19 13:47:20 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,11 +2,25 @@ function myFunction(){
   document.getElementById("meredith").innerHTML = "Scroll Down for More About Me!";
 }
 
-window.alert("Welcome to My Webpage!")
 
-function MouseRollover(MyImage) {
-  MyImage.src = "goldendoodle puppy";
+function aboutMe() {
+  var x = document.getElementById("moreMe");
+
+window.alert("Welcome to My Webpage!");
+
+// function MouseRollover(MyImage) {
+//   MyImage.src = "goldendoodle puppy";
+// }
+// function MouseOut(MyImage) {
+// MyImage.src = "goldendoodle puppy orig.";
+// }
+
+function test(){
+  document.getElementById("puppy").style.height="200px";
+  document.getElementById("puppy").style.width="232px";
 }
-function MouseOut(MyImage) {
-MyImage.src = "puppy";
+
+function test2(){
+  document.getElementById("puppy").style.height="100px";
+  document.getElementById("puppy").style.width="132px";
 }
